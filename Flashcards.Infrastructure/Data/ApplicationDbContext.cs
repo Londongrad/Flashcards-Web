ebbf34e32a713e41fdc5edaaf98f6d81afc6e36d
Londongrad <h-b-12@mail.ru>
2025-03-23 15:53:21 +0900

    Insignificant corrections №11

--- Flashcards.Infrastructure/Data/ApplicationDbContext.cs
+++ Flashcards.Infrastructure/Data/ApplicationDbContext.cs
@@ -13,7 +13,7 @@ namespace Flashcards.Infrastructure.Data
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
         {
-            Database.EnsureCreated();
+           Database.EnsureCreated();
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
