3801ac07b9757a72648bb6e52800047d2a211cf9
vanostalker236@gmail.com <vanostalker236@gmail.com>
2025-06-05 23:19:20 +0900

    TTS operations are currently in progress.

--- Flashcards.Web/Areas/Account/Controllers/AccountController.cs
+++ Flashcards.Web/Areas/Account/Controllers/AccountController.cs
@@ -3,7 +3,6 @@ using Flashcards.Web.Areas.Account.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
 
 namespace Flashcards.Web.Areas.Account.Controllers
 {
